b292accfeb840e323a9046f933a4afb2f8264df9
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-06-25 16:59:38 -0600

    feat(services): 🤡 Addition of the type commit tags to the types mockup.

--- src/services/mockups/emojis.js
+++ src/services/mockups/emojis.js
@@ -1,294 +1,345 @@
 export const emojis = [
   {
     icon: '',
-    label: 'Sin emoji'
+    label: 'Sin emoji',
+    tags: []
   },
   {
     icon: '🎨',
-    label: 'Mejora de la estructura y/o el formato del código'
+    label: 'Mejora de la estructura y/o el formato del código',
+    tags: ['styles']
   },
   {
     icon: '⚡',
-    label: 'Mejora del rendimiento'
+    label: 'Mejora del rendimiento de la aplicacion',
+    tags: ['perf']
   },
   {
     icon: '🔥',
-    label: 'Eliminar código o archivos'
+    label: 'Eliminacion de código o archivos',
+    tags: ['fix', 'revert', 'refactor']
   },
   {
     icon: '🐛',
-    label: 'Corregir un error'
+    label: 'Correccion de un error o bug',
+    tags: ['fix']
   },
   {
     icon: '🚑',
-    label: 'Revisión crítica'
+    label: 'Revisión crítica de un error o bug',
+    tags: ['fix']
   },
   {
     icon: '✨',
-    label: 'Introducir nuevas funciones'
+    label: 'Introduccion de nueva(s) funciones',
+    tags: ['feat']
   },
   {
     icon: '📝',
-    label: 'Añadir o actualizar documentación'
+    label: 'Adicion o actualizacion de la documentación',
+    tags: ['docs']
   },
   {
     icon: '🚀',
-    label: 'Desplegar cosas (deploy)'
+    label: 'Despliegue de cosas (deploy)',
+    tags: ['build']
   },
   {
     icon: '💄',
-    label: 'Añadir y/o actualizar la interfaz de usuario y/o los archivos de estilo'
+    label: 'Adicion y/o actualizacion de la interfaz de usuario y/o los archivos CSS',
+    tags: ['feat']
   },
   {
     icon: '🎉',
-    label: 'Iniciar un proyecto (first commit)'
+    label: 'Inicio de un proyecto (first commit)',
+    tags: ['feat']
   },
   {
     icon: '✅',
-    label: 'Añadir, actualizar o pasar pruebas (tests)'
+    label: 'Añadir, actualizar o pasar pruebas (tests)',
+    tags: ['test']
   },
   {
     icon: '🔓',
-    label: 'Solucionar los problemas de seguridad'
+    label: 'Solucionar los problemas de seguridad',
+    tags: ['fix']
   },
   {
     icon: '🔐',
-    label: 'Añadir o actualizar secretos (.env)'
+    label: 'Añadir o actualizar secretos (.env)',
+    tags: ['build', 'chore']
   },
   {
     icon: '🏷️',
-    label: 'Generar una nueva version (Semantic Version)'
+    label: 'Generar una nueva version (Semantic Version)',
+    tags: ['feat']
   },
   {
     icon: '🚨',
-    label: 'Corregir las advertencias del compilador/linter'
+    label: 'Corregir las advertencias del compilador/linter',
+    tags: ['fix', 'refactor']
   },
   {
     icon: '🚧',
-    label: 'Tarea en curso/pausada'
+    label: 'Tarea en curso/pausada',
+    tags: ['feat']
   },
   {
     icon: '💚',
-    label: 'Corregir la compilación CI'
+    label: 'Corregir la compilación CI',
+    tags: ['ci']
   },
   {
     icon: '⤵️',
-    label: 'Bajar dependencias (version inferior)'
+    label: 'Bajar dependencias (version inferior)',
+    tags: ['build']
   },
   {
     icon: '⤴️',
-    label: 'Actualizar dependencias (version superior)'
+    label: 'Actualizar dependencias (version superior)',
+    tags: ['build']
   },
   {
     icon: '📌',
-    label: 'Fijar dependencias a versiones concretas'
+    label: 'Fijar dependencias a versiones concretas',
+    tags: ['build']
   },
   {
     icon: '👷',
-    label: 'Añadir o actualizar el sistema de construcción CI'
+    label: 'Añadir o actualizar el sistema de construcción CI',
+    tags: ['ci']
   },
   {
     icon: '📈',
-    label: 'Añada o actualice los análisis o el código de seguimiento'
+    label: 'Añadir o actualizar los análisis o el código de seguimiento',
+    tags: ['test']
   },
   {
     icon: '♻️',
-    label: 'Refactorizacion de código'
+    label: 'Refactorizacion de código',
+    tags: ['refactor']
   },
   {
     icon: '➕',
-    label: 'Añadir una dependencia'
+    label: 'Añadir una dependencia',
+    tags: ['build']
   },
   {
     icon: '➖',
-    label: 'Eliminar una dependencia'
+    label: 'Eliminar una dependencia',
+    tags: ['build']
   },
   {
     icon: '🔧',
-    label: 'Añadir o actualizar archivos de configuración'
+    label: 'Añadir o actualizar archivos de configuración',
+    tags: ['build']
   },
   {
     icon: '🔨',
-    label: 'Añadir o actualizar scripts de desarrollo'
+    label: 'Añadir o actualizar scripts de desarrollo',
+    tags: ['feat']
   },
-  {
-    icon: '🌐',
-    label: 'Internacionalización y localización'
-  },
+  // {
+  //   icon: '🌐',
+  //   label: 'Internacionalización y localización',
+  //   tags: ['feat']
+  // },
   {
     icon: '✏️',
-    label: 'Corregir errores tipográficos (ortografia)'
+    label: 'Corregir errores tipográficos (ortografia)',
+    tags: ['fix']
   },
   {
     icon: '💩',
-    label: 'Escribir código defectuoso que debe mejorarse'
+    label: 'Escribir código defectuoso que debe mejorarse',
+    tags: ['refactor']
   },
   {
     icon: '⏪',
-    label: 'Revertir cambios'
+    label: 'Revertir cambios',
+    tags: ['revert']
   },
   {
     icon: '🔀',
-    label: 'Fusionar ramas (merge)'
+    label: 'Fusionar ramas (merge)',
+    tags: ['build']
   },
-  {
-    icon: '📦',
-    label: 'Añadir o actualizar archivos o paquetes compilados'
-  },
+  // {
+  //   icon: '📦',
+  //   label: 'Añadir o actualizar archivos o paquetes compilados',
+  //   tags: ['build']
+  // },
   {
     icon: '👽',
-    label: 'Actualizar el código debido a cambios en la API externa'
+    label: 'Actualizar el código debido a cambios en la API externa',
+    tags: ['refactor']
   },
   {
     icon: '🚚',
-    label: 'Mover o renombrar recursos (archivos, rutas, modulos, etc)'
+    label: 'Mover o renombrar recursos (archivos, rutas, modulos, etc)',
+    tags: ['refactor']
   },
   {
     icon: '📄',
-    label: 'Añadir o actualizar licencia'
+    label: 'Añadir o actualizar licencia',
+    tags: ['chore']
   },
   {
     icon: '💥',
-    label: 'Introducir cambios importantes (MAJOR)'
+    label: 'Introducir cambios importantes (MAJOR)',
+    tags: ['feat']
   },
   {
     icon: '🖼️',
-    label: 'Añadir o actualizar recursos (assets)'
+    label: 'Añadir o actualizar recursos (assets)',
+    tags: ['feat']
   },
   {
     icon: '♿',
-    label: 'Mejorar la accesibilidad'
+    label: 'Mejorar la accesibilidad',
+    tags: ['feat']
   },
   {
     icon: '💡',
-    label: 'Añadir o actualizar comentarios en el código fuente'
-  },
-  {
-    icon: '🍻',
-    label: 'Escribir código ebrio'
+    label: 'Añadir o actualizar comentarios en el código fuente',
+    tags: ['docs']
   },
   {
     icon: '💬',
-    label: 'Añadir o actualizar texto y literales'
+    label: 'Añadir o actualizar texto y literales',
+    tags: ['feat']
   },
   {
     icon: '🗃️',
-    label: 'Realizar cambios relacionados con la base de datos'
+    label: 'Realizar cambios relacionados con la base de datos',
+    tags: ['feat']
   },
   {
     icon: '🔊',
-    label: 'Añadir o actualizar registros (logs)'
+    label: 'Añadir o actualizar registros (logs)',
+    tags: ['chore']
   },
   {
     icon: '🔇',
-    label: 'Eliminar registros (logs)'
+    label: 'Eliminar registros (logs)',
+    tags: ['chore']
   },
   {
     icon: '👥',
-    label: 'Añadir o actualizar colaborador(es)'
+    label: 'Añadir o actualizar colaborador(es)',
+    tags: ['chore']
   },
   {
     icon: '🚸',
-    label: 'Mejorar la experiencia y /o usabilidad del usuario'
+    label: 'Mejora de la experiencia y /o usabilidad del usuario',
+    tags: ['feat']
   },
   {
     icon: '🏗️',
-    label: 'Realizar cambios arquitectónicos (arquitectura)'
+    label: 'Realizar cambios arquitectónicos (arquitectura)',
+    tags: ['build']
   },
   {
     icon: '📱',
-    label: 'Trabajar en el diseño adaptativo'
+    label: 'Trabajar en el diseño adaptativo',
+    tags: ['feat']
   },
   {
     icon: '🤡',
-    label: 'Simular cosas (mocks)'
+    label: 'Simular cosas (mocks)',
+    tags: ['feat']
   },
   {
     icon: '🥚',
-    label: 'Añadir o actualizar un fichero .gitignore'
-  },
-  {
-    icon: '📸',
-    label: 'Añadir o actualizar instantáneas'
-  },
-  {
-    icon: '⚗️',
-    label: 'Realización de experimentos'
+    label: 'Añadir o actualizar un fichero .gitignore',
+    tags: ['chore']
   },
   {
     icon: '🔍',
-    label: 'Mejorar el SEO'
+    label: 'Mejorar el SEO',
+    tags: ['feat']
   },
   {
     icon: '🔖',
-    label: 'Añadir o actualizar tipos'
+    label: 'Añadir o actualizar fuentes (CSS)',
+    tags: ['feat']
   },
-  {
-    icon: '🌱',
-    label: 'Añadir o actualizar ficheros semilla'
-  },
-  {
-    icon: '⛳',
-    label: 'Añadir, actualizar o eliminar banderas de características'
-  },
+  // {
+  //   icon: '🌱',
+  //   label: 'Añadir o actualizar ficheros semilla'
+  // },
+  // {
+  //   icon: '⛳',
+  //   label: 'Añadir, actualizar o eliminar banderas de características'
+  // },
   {
     icon: '🥅',
-    label: 'Captura de errores.'
+    label: 'Captura de errores.',
+    tags: ['fix']
   },
   {
     icon: '💫',
-    label: 'Añadir o actualizar animaciones y transiciones'
+    label: 'Añadir o actualizar animaciones y transiciones',
+    tags: ['feat']
   },
   {
     icon: '🗑️',
-    label: 'Código obsoleto que hay que limpiar'
+    label: 'Código obsoleto que hay que limpiar',
+    tags: ['refactor']
   },
   {
     icon: '🛂',
-    label: 'Trabajar en código relacionado con autorizaciones, funciones y permisos'
+    label: 'Trabajar en código relacionado con autorizaciones, funciones y permisos',
+    tags: ['feat']
   },
   {
     icon: '🩹',
-    label: 'Solución sencilla de un problema no crítico'
+    label: 'Solución sencilla de un problema no crítico',
+    tags: ['fix']
   },
-  {
-    icon: '🧐',
-    label: 'Exploración / inspección de datos'
-  },
+  // {
+  //   icon: '🧐',
+  //   label: 'Exploración / inspección de datos'
+  // },
   {
     icon: '⚰️',
-    label: 'Eliminar código muerto'
+    label: 'Eliminar código muerto',
+    tags: ['refactor']
   },
-  {
-    icon: '🧪',
-    label: 'Añadir una prueba fallida'
-  },
+  // {
+  //   icon: '🧪',
+  //   label: 'Añadir una prueba fallida'
+  // },
   {
     icon: '👔',
-    label: 'Añadir o actualizar la lógica de negocio'
+    label: 'Añadir o actualizar la lógica de negocio',
+    tags: ['feat']
   },
-  {
-    icon: '🩺',
-    label: 'Añadir o actualizar el chequeo'
-  },
+  // {
+  //   icon: '🩺',
+  //   label: 'Añadir o actualizar el chequeo'
+  // },
   {
     icon: '🧱',
-    label: 'Cambios relacionados con las infraestructuras'
+    label: 'Cambios relacionados con las infraestructuras',
+    tags: ['build']
   },
-  {
-    icon: '🧑‍💻',
-    label: 'Mejorar la experiencia de los desarrolladores'
-  },
-  {
-    icon: '💸',
-    label: 'Añadir patrocinios o infraestructuras relacionadas con el dinero'
-  },
+  // {
+  //   icon: '🧑‍💻',
+  //   label: 'Mejorar la experiencia de los desarrolladores'
+  // },
+  // {
+  //   icon: '💸',
+  //   label: 'Añadir patrocinios o infraestructuras relacionadas con el dinero'
+  // },
   {
     icon: '🧵',
-    label: 'Añadir o actualizar código relacionado con multithreading o concurrencia'
+    label: 'Añadir o actualizar código relacionado con multithreading o concurrencia',
+    tags: ['feat']
   },
   {
     icon: '🦺',
-    label: 'Añadir o actualizar código relacionado con la validación'
+    label: 'Añadir o actualizar código relacionado con la validación',
+    tags: ['feat']
   }
 ]
