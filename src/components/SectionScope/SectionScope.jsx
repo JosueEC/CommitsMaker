a0142c691b7a824ddaabce623571e4fb45be163e
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-06-25 12:04:25 -0600

    feat(components): 🔨 Addition of the events onChange, onClick and onKeyDown to the SectionScope component.

--- src/components/SectionScope/SectionScope.jsx
+++ src/components/SectionScope/SectionScope.jsx
@@ -1,12 +1,38 @@
 /* eslint-disable react/prop-types */
 import './SectionScope.css'
+import { useState } from 'react'
 import OptionScope from '../OptionScope/OptionScope'
 
 export default function SectionScope ({ textHead, scopes }) {
+  const [newScope, setNewScope] = useState('')
+
+  function handleChange (event) {
+    const text = event.target.value
+    setNewScope(text)
+  }
+
+  function handleAddScope (event) {
+    scopes.push(newScope)
+  }
+
+  function handleEnterPressed (event) {
+    const key = event.target.key
+    if (key === 'Enter') {
+      handleAddScope()
+    }
+  }
+
   return (
     <div className='sectionScope'>
       <div className='head'>
         <p>{textHead}</p>
+        <input
+          type='text'
+          placeholder='Añadir un nuevo scope'
+          onChange={handleChange}
+          onKeyDown={handleEnterPressed}
+        />
+        <button onClick={handleAddScope}>Añadir</button>
       </div>
       <div className='content'>
         {
